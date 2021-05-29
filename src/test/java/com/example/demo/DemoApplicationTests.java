4f11b0b0da6066cceefd3e5f8c9b753a21f47553
ilia <ilia.murometc@gmail.com>
2021-05-29 14:21:00 +0300

    added dependencies added sending XML and JSON object added some tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Disabled @Test
+	public void should_return_simple_string_on_get_request() {
+	when().
+		get("/users/simple/{id}", 1).
+	then()
+		.statusCode(200)
+		.body(equalTo("get user was called with userId = 1"));
 	}
 
+	//http://localhost:8080/users?page=1&limit=50&sort=bla-bla
+	@Disabled @Test
+	public void should_return_demo_string_using_annRequestParam(){
+		when().
+				get("/users?page={page}&limit={limit}&sort={sort}", 1, 50, "bla-bla").
+		then().
+				statusCode(200).
+				body(equalTo("get users was called with page = 1 and limit = 50 and sort = bla-bla"));
+	}
 }
